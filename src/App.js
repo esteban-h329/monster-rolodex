e9cfb120328f9313ff35497380d94fcec9d387f3
Esteban <esteban9259@hotmail.com>
2022-04-14 11:33:48 -0500

    change to a class component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
