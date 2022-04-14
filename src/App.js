8915ed3e51fc3df720bbf240c60253a7b5d98f09
Esteban <esteban9259@hotmail.com>
2022-04-14 11:56:46 -0500

    create a constructor an acces from render

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Esteban",
+    };
+  }
+
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <p>{this.state.name}</p>
+      </div>
+    );
   }
 }
 
