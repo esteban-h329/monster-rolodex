93ac8a4d3820ce438fa00c813aadfa25137103c1
Esteban <esteban9259@hotmail.com>
2022-04-14 12:23:29 -0500

    Shallow merge - Events

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+Monster Rolodex
 
+I just wanna record all the process learning react and create this basic project of a SPA and practice all of this topics.
+
+*JSX
+*Render
+*Function components
+*class components
+*Life cycle components
+*Fetch
+*Promises
+*Optimizations
+\*Events
+
+-
--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Esteban",
+      name: { firstName: "Esteban", lastName: "Montoya" },
+      company: "ZTM",
     };
   }
 
   render() {
     return (
       <div className="App">
-        <p>{this.state.name}</p>
+        <p>
+          Hi, I am {this.state.name.firstName} {this.state.name.lastName} and I
+          work at {this.state.company}
+        </p>
+        <button
+          onClick={() => {
+            this.setState(() => {
+              return {
+                name: { firstName: "Emmanuel", lastName: "Hernández" },
+              };
+            });
+          }}
+        >
+          Change the name
+        </button>
       </div>
     );
   }
